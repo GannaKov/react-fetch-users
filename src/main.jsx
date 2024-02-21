8fe610f44034c5c40580cffe9c392f864c6c260b
GannaKov <zlatta2000@gmail.com>
2024-02-21 18:05:14 +0100

    ифсл ещ HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
@@ -9,11 +9,11 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-fetch-users">
-      {/* basename="/your_repo_name/" */}
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    {/* <BrowserRouter basename="/react-fetch-users"> */}
+
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
