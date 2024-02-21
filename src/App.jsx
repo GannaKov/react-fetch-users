ce6ace6fb40e03f9ffd1c2b58faf44bc3735723a
GannaKov <zlatta2000@gmail.com>
2024-02-21 17:56:16 +0100

    "build": "vite build --base /react-fetch-users",

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom"; //
-import { HashRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import UsersPage from "./pages/UsersPage/UsersPage";
 import SingleUserPage from "./pages/SingleUserPage/SingleUserPage";
@@ -11,20 +11,21 @@ import SharedLayout from "./components/SharedLayout/SharedLayout";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/users/:id" element={<SingleUserPage />}>
-            <Route path="address" element={<Address />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
+    // <Router>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/:id" element={<SingleUserPage />}>
+          <Route path="address" element={<Address />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
-      </Routes>
-    </Router>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+    // </Router>
   );
 };
 
 export default App;
+// "build": "vite build",
