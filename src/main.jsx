c96c1305c97ded676e50e93443f823842aa680f2
GannaKov <zlatta2000@gmail.com>
2024-02-21 17:32:42 +0100

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-fetch-users/">
+    <BrowserRouter basename="/react-fetch-users">
       {/* basename="/your_repo_name/" */}
       <ChakraProvider>
         <App />
